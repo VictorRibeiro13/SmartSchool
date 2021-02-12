5343c9aa33c2a298e9445b8d6bf9023a874a2485
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-02-11 23:30:22 -0300

    feat: add SmarSchool Data context and entity config

---
+++ SmarSchool.API/Data/SmartSchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SmarSchool.API.Models;
+
+namespace SmartSchool.API.Data {
+    public class SmartSchoolContext : DbContext 
+    {
+        public SmartSchoolContext(DbContextOptions<SmartSchoolContext> options) : base(options) {}
+
+        public DbSet<Student> students { get; set; }
+        public DbSet<Teacher> teachers { get; set; }
+        public DbSet<Subject> subjects { get; set; }
+        public DbSet<StudentSubject> studentsSubjects { get; set; }
+
+
+        protected override void OnModelCreating (ModelBuilder builder)
+        {
+            builder.Entity<StudentSubject>().HasKey(AD => new { AD.StudentId, AD.SubjectId });
+        }
+    }
+}
--- SmarSchool.API/Startup.cs
+++ SmarSchool.API/Startup.cs
@@ -1,14 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SmartSchool.API.Data;
 
 namespace SmarSchool.API {
     public class Startup {
@@ -20,6 +17,11 @@ namespace SmarSchool.API {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+            // Adding context file at the project
+            services.AddDbContext<SmartSchoolContext>(
+                context => context.UseSqlite(Configuration.GetConnectionString("Sqlite"))
+            );
+
             services.AddControllers();
         }
 
